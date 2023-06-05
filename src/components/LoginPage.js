bb245dec99dedecbb8bdd45cf03aa5c3ffb64bc1
Shi Hong <68313416+chuashihong@users.noreply.github.com>
2023-06-05 17:29:32 +0800

    delete unused code

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -44,32 +44,6 @@ export const LoginPage = () => {
         }
     }
     return (
-        // <div className="flex justify-center items-center h-[100vh] w-[100vw] bg-gray-200">
-        // <div className="login-container">
-        //     <h2 className="text-xl font-bold p-2 mb-6">Login with your account</h2>
-        //     <form className="" onSubmit={handleLogin}>
-        //         <div className="flex">
-        //             <div className="flex flex-col pr-2">
-        //                 <div className="flex items-center justify-end h-full">
-        //                     <label className="">Email:</label>
-        //                 </div>
-        //                 <div className="flex items-center justify-end h-full">
-        //                     <label className="">Password:</label>
-        //                 </div>
-        //             </div>
-        //             <div className="flex flex-col">
-        //                 <input className="" type="email" value={email} onChange={handleEmailChange} />
-        //                 <input type="password" value={password} onChange={handlePasswordChange} />
-        //             </div>
-        //         </div>
-        //         <div className="mt-4">
-        //             <button className="button-green rounded" type="submit">Login</button>
-        //         </div>
-        //     </form>
-        //     {error && <div className="error-message">{error}</div>}
-        // </div>
-        // <button onClick={handleSignUp} class="">Sign Up</button>
-        // </div>
         <div className="flex justify-center items-center h-[100vh] w-[100vw] bg-gray-200">
             <div className="login-container">
                 <h2 className="text-xl font-bold p-2 mb-6">Login with your account</h2>
@@ -95,8 +69,7 @@ export const LoginPage = () => {
                         <button className="button-green rounded" type="submit">Login</button>
                     </div>
                 </form>
-                {error && <div className="error-message">{error}</div>}
-
+                {error && <div className="error-message">{error}</div>} 
             </div>
         </div>
     )
