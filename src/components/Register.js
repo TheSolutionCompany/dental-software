0dc2fb115c1c627c0275384fa9f72cc7aaf68906
ngshijun <shijunng2010@gmail.com>
2023-06-06 19:02:17 +0800

    implement search

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,19 +1,34 @@
 import React, { useState } from "react"
 import Modal from "react-modal"
+import { db } from "../firebase"
+import { collection, query, where, getDocs } from "firebase/firestore"
 
 Modal.setAppElement("#root")
 
 export const Register = () => {
+    var q = query(collection(db, "Patients"))
     const [isOpen, setIsOpen] = useState(false)
     const [searchQuery, setSearchQuery] = useState("")
+    const [patientsList, setPatientsList] = useState([])
 
     const toggleModal = () => {
         setIsOpen(!isOpen)
     }
 
-    const handleQueryChange = (event) => {
+    const handleQueryChange = async (event) => {
+        event.preventDefault()
         setSearchQuery(event.target.value)
-        console.log(searchQuery)
+
+        if (searchQuery) {
+            const start = searchQuery
+            const end = start.replace(/.$/, (c) => String.fromCharCode(c.charCodeAt(0) + 1))
+
+            q = query(q, where("name", ">=", start), where("name", "<", end))
+            const result = (await getDocs(q)).docs.map((doc) => doc.data().name)
+            setPatientsList(Object.values(result))
+        } else {
+            setPatientsList([])
+        }
     }
 
     return (
@@ -25,7 +40,14 @@ export const Register = () => {
                     <label>Search Field:</label>
                     <input type="text" onChange={handleQueryChange} />
                 </div>
-                <button onClick={toggleModal}>Close modal</button>
+                <button className="" onClick={toggleModal}>
+                    Close
+                </button>
+                <ul>
+                    {patientsList.map((patient) => (
+                        <li>{patient}</li>
+                    ))}
+                </ul>
             </Modal>
         </div>
     )
