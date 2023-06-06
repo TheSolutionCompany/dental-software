9c34d4d0ca44b68cae0b700a808a879525524c9f
ngshijun <shijunng2010@gmail.com>
2023-06-07 00:42:05 +0800

    finish real time search

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,42 +1,46 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Modal from "react-modal"
 import { db } from "../firebase"
 import { collection, query, where, getDocs } from "firebase/firestore"
 
-Modal.setAppElement("#root");
+Modal.setAppElement("#root")
 
 export const Register = () => {
-    var q = query(collection(db, "Patients"))
     const [isOpen, setIsOpen] = useState(false)
     const [searchQuery, setSearchQuery] = useState("")
     const [patientsList, setPatientsList] = useState([])
 
+    useEffect(() => {
+        const handleSearch = async () => {
+            if (searchQuery) {
+                const start = searchQuery
+                const end = start.replace(/.$/, (c) => String.fromCharCode(c.charCodeAt(0) + 1))
+                const q = query(collection(db, "Patients"), where("name", ">=", start), where("name", "<", end))
+                const result = (await getDocs(q)).docs.map((doc) => doc.data().name)
+                setPatientsList(Object.values(result.sort()))
+            } else {
+                setPatientsList([])
+            }
+        }
+
+        handleSearch()
+    }, [searchQuery])
+
     const toggleModal = () => {
-        setIsOpen(!isOpen);
-    };
+        setIsOpen(!isOpen)
+    }
 
     const handleQueryChange = async (event) => {
-        event.preventDefault()
         setSearchQuery(event.target.value)
-
-        if (searchQuery) {
-            const start = searchQuery
-            const end = start.replace(/.$/, (c) => String.fromCharCode(c.charCodeAt(0) + 1))
-
-            q = query(q, where("name", ">=", start), where("name", "<", end))
-            const result = (await getDocs(q)).docs.map((doc) => doc.data().name)
-            setPatientsList(Object.values(result))
-        } else {
-            setPatientsList([])
-        }
     }
 
     return (
         <div className="App">
             <li className="cursor-pointer select-none">
-                <button  
+                <button
                     onClick={toggleModal}
-                    class="flex w-full items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    class="flex w-full items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
                     <svg
                         aria-hidden="true"
                         class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -46,22 +50,16 @@ export const Register = () => {
                     >
                         <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
                     </svg>
-                    <span class="text-left flex-1 ml-3 whitespace-nowrap">
-                        Register
-                    </span>
+                    <span class="text-left flex-1 ml-3 whitespace-nowrap">Register</span>
                     {/* <span class="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
                                 Pro
                             </span> */}
                 </button>
             </li>
-            <Modal
-                isOpen={isOpen}
-                onRequestClose={toggleModal}
-                contentLabel="Register"
-            >
+            <Modal isOpen={isOpen} onRequestClose={toggleModal} contentLabel="Register">
                 <div>
                     <label>Search Field:</label>
-                    <input type="text" onChange={handleQueryChange} />
+                    <input type="text" defaultValue={""} onChange={handleQueryChange} />
                 </div>
                 <button className="" onClick={toggleModal}>
                     Close
@@ -73,7 +71,7 @@ export const Register = () => {
                 </ul>
             </Modal>
         </div>
-    );
-};
+    )
+}
 
-export default Register;
+export default Register
