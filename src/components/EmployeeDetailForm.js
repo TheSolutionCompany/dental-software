2674a67d0ce86277eac7552332e382efdeb52db0
Chong Jing Quan <jingquan0302@gmail.com>
2023-06-30 17:11:23 +0800

    Remove edit mode from employee form

--- src/components/EmployeeDetailForm.js
+++ src/components/EmployeeDetailForm.js
@@ -9,30 +9,27 @@ Modal.setAppElement("#root");
 
 export default function EmployeeDetailForm(props) {
     const [isOpen, setIsOpen] = useState(false);
-    const { employee, addEmployee, editEmployee } = useDatabase();
+    const { addEmployee } = useDatabase();
 
     const toggleModal = () => {
         setIsOpen(!isOpen);
     };
 
-    const editMode = props.data.editMode;
-    const activeEmployee = props.data.activeEmployee;
+    const [displayName, setDisplayName] = useState("");
+    const [isDisplayNameValid, setIsDisplayNameValid] = useState(false);
 
-    const [displayName, setDisplayName] = useState(editMode ? activeEmployee.displayName : "");
-    const [isDisplayNameValid, setIsDisplayNameValid] = useState(editMode);
-
-    const [email, setEmail] = useState(editMode ? activeEmployee.email : "");
-    const [isEmailValid, setIsEmailValid] = useState(editMode);
-    const [isEmailEmpty, setIsEmailEmpty] = useState(!editMode);
+    const [email, setEmail] = useState("");
+    const [isEmailValid, setIsEmailValid] = useState(false);
+    const [isEmailEmpty, setIsEmailEmpty] = useState(false);
 
     const [password, setPassword] = useState("");
     const [isPasswordValid, setIsPasswordValid] = useState(false);
 
-    const [position, setPosition] = useState(editMode ? activeEmployee.position : "Doctor");
+    const [position, setPosition] = useState("Doctor");
 
     const [isValidInput, setIsValidInput] = useState(false);
 
-    const title = editMode ? "Edit Employee" : "Add New Employee";
+    const title = "Add New Employee";
 
     const smallModal = {
         content: {
@@ -49,70 +46,38 @@ export default function EmployeeDetailForm(props) {
 
         document.getElementById("name").disabled = true;
         document.getElementById("position").disabled = true;
-
-        if (editMode) {
-            if (await editEmployee(activeEmployee.id, displayName, position)) {
-                toggleModal();
-                toast.success("Employee edited successfully", {
-                    position: "top-center",
-                    autoClose: 1000,
-                    hideProgressBar: true,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                });
-                toast.clearWaitingQueue();
-            } else {
-                document.getElementById("name").disabled = false;
-                document.getElementById("email").disabled = false;
-                toast.error("Failed to edit item. Please try again later.", {
-                    position: "top-center",
-                    autoClose: 1000,
-                    hideProgressBar: true,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                });
-                toast.clearWaitingQueue();
-            }
+        document.getElementById("email").disabled = true;
+        document.getElementById("password").disabled = true;
+        if (await addEmployee(displayName, email, position, password)) {
+            toggleModal();
+            toast.success("Employee added successfully", {
+                position: "top-center",
+                autoClose: 1000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+            toast.clearWaitingQueue();
+            resetForm();
         } else {
-            document.getElementById("email").disabled = true;
-            document.getElementById("password").disabled = true;
-            if (await addEmployee(displayName, email, position, password)) {
-                toggleModal();
-                toast.success("Employee added successfully", {
-                    position: "top-center",
-                    autoClose: 1000,
-                    hideProgressBar: true,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                });
-                toast.clearWaitingQueue();
-                resetForm();
-            } else {
-                document.getElementById("name").disabled = false;
-                document.getElementById("email").disabled = false;
-                document.getElementById("position").disabled = false;
-                document.getElementById("password").disabled = false;
-                toast.error("Failed to add item. Please try again later.", {
-                    position: "top-center",
-                    autoClose: 1000,
-                    hideProgressBar: true,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                });
-                toast.clearWaitingQueue();
-            }
+            document.getElementById("name").disabled = false;
+            document.getElementById("email").disabled = false;
+            document.getElementById("position").disabled = false;
+            document.getElementById("password").disabled = false;
+            toast.error("Failed to add item. Please try again later.", {
+                position: "top-center",
+                autoClose: 1000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+            toast.clearWaitingQueue();
         }
     };
 
@@ -127,13 +92,13 @@ export default function EmployeeDetailForm(props) {
         let isNameNonEmpty = displayName !== "" && displayName !== null && displayName !== undefined;
         setIsDisplayNameValid(isNameNonEmpty);
 
-        let isEmailNonEmpty = editMode || (email !== "" && email !== null && email !== undefined);
+        let isEmailNonEmpty = email !== "" && email !== null && email !== undefined;
         setIsEmailEmpty(!isEmailNonEmpty);
         let isEmailValid = isEmailNonEmpty && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
         setIsEmailValid(isEmailValid);
 
         // pls change to regex tqvm
-        let isPasswordValid = editMode || (password !== "" && password !== null && password !== undefined);
+        let isPasswordValid = password !== null && password !== undefined && password.length >= 6;
         setIsPasswordValid(isPasswordValid);
 
         setIsValidInput(!(isNameNonEmpty && isEmailValid && isPasswordValid));
@@ -185,7 +150,7 @@ export default function EmployeeDetailForm(props) {
                                 <option value="Administrator">Administrator</option>
                             </select>
 
-                            <label style={{ marginTop: "20px" }} hidden={editMode}>Email</label>
+                            <label style={{ marginTop: "20px" }}>Email</label>
                             <input
                                 id="email"
                                 value={email}
@@ -194,18 +159,15 @@ export default function EmployeeDetailForm(props) {
                                 }}
                                 required
                                 type="email"
-                                hidden={editMode}
                             />
-                            <p hidden={!isEmailEmpty || editMode} style={{ fontSize: "12px" }}>
+                            <p hidden={!isEmailEmpty} style={{ fontSize: "12px" }}>
                                 Please fill in this field.
                             </p>
-                            <p hidden={isEmailEmpty || isEmailValid || editMode} style={{ fontSize: "12px" }}>
+                            <p hidden={isEmailEmpty || isEmailValid} style={{ fontSize: "12px" }}>
                                 The format of the email is not correct.
                             </p>
 
-                            <label hidden={editMode} style={{ marginTop: "20px" }}>
-                                Password
-                            </label>
+                            <label style={{ marginTop: "20px" }}>Password</label>
                             <input
                                 id="password"
                                 value={password}
@@ -213,10 +175,9 @@ export default function EmployeeDetailForm(props) {
                                     setPassword(e.target.value);
                                 }}
                                 type="password"
-                                hidden={editMode}
                             />
-                            <p hidden={isPasswordValid || editMode} style={{ fontSize: "12px" }}>
-                                The password must satisfy the following requirements: ...
+                            <p hidden={isPasswordValid} style={{ fontSize: "12px" }}>
+                                The password must have at least 6 characters.
                             </p>
 
                             <button
