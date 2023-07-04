6692c4ca5e64dc51bb67010e1635326a4392ca56
ngshijun <shijunng2010@gmail.com>
2023-07-05 00:43:16 +0800

    minor change

--- src/pages/Employee.js
+++ src/pages/Employee.js
@@ -1,36 +1,36 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
-import Header from "../components/Header";
-import SideBar from "../components/SideBar";
-import { useDatabase } from "../contexts/DatabaseContext";
-import DeleteConfirmation from "../components/DeleteConfirmation";
-import EmployeeDetailForm from "../components/EmployeeDetailForm";
-import { encode, verify } from "../util/JwtUtil";
-import WorkingHourForm from "../components/WorkingHourForm";
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useAuth } from "../contexts/AuthContext"
+import Header from "../components/Header"
+import SideBar from "../components/SideBar"
+import { useDatabase } from "../contexts/DatabaseContext"
+import DeleteConfirmation from "../components/DeleteConfirmation"
+import EmployeeDetailForm from "../components/EmployeeDetailForm"
+import { encode, verify } from "../util/JwtUtil"
+import WorkingHourForm from "../components/WorkingHourForm"
 
 export default function Employee() {
-    const navigate = useNavigate();
-    const { logout } = useAuth();
+    const navigate = useNavigate()
+    const { logout } = useAuth()
 
-    const { employees } = useDatabase();
+    const { employees } = useDatabase()
 
     async function handleLogout() {
         try {
-            await logout();
-            navigate("/");
+            await logout()
+            navigate("/")
         } catch (error) {
-            console.log(error);
+            console.log(error)
         }
     }
 
     function generateEmployeeRows(employeeTable) {
         return employeeTable.map((employeeRow) => (
-            <tr className="table-tr-tbody-gray" key={employeeRow.id}>
-                <td className="table-td-gray w-[30%]">{employeeRow.data().displayName}</td>
-                <td className="table-td-gray w-[30%]">{employeeRow.data().email}</td>
-                <td className="table-td-gray w-[10%]">{employeeRow.data().position}</td>
-                <td className="table-td-gray w-[10%]">
+            <tr key={employeeRow.id}>
+                <td className="w-[30%]">{employeeRow.data().displayName}</td>
+                <td className="w-[30%]">{employeeRow.data().email}</td>
+                <td className="w-[10%]">{employeeRow.data().position}</td>
+                <td className="w-[10%]">
                     <WorkingHourForm
                         activeEmployee={{
                             id: employeeRow.id,
@@ -38,25 +38,25 @@ export default function Employee() {
                         }}
                     />
                 </td>
-                <td className="table-td-gray w-[10%]">
+                <td className="w-[10%]">
                     <DeleteConfirmation
                         docName={"users"}
                         activeItemId={employeeRow.id}
                         activeItemName={employeeRow.data().displayName}
                     />
                 </td>
-                <th className="table-th-gray w-[10%]">
+                <th className="w-[10%]">
                     <button onClick={() => showToken(employeeRow.data(), employeeRow.id)}>Generate Token</button>
                 </th>
             </tr>
-        ));
+        ))
     }
 
     async function showToken(employee, id) {
-        let jwt = await encode({ name: employee.displayName, email: employee.email, position: employee.position }, id);
-        alert(jwt);
-        let decrypted = await verify(jwt, id);
-        console.log(decrypted);
+        let jwt = await encode({ name: employee.displayName, email: employee.email, position: employee.position }, id)
+        alert(jwt)
+        let decrypted = await verify(jwt, id)
+        console.log(decrypted)
     }
 
     return (
@@ -69,19 +69,21 @@ export default function Employee() {
                     <EmployeeDetailForm data={{ editMode: false, activeEmployee: null }} />
                     <div className="flex flex-col pt-10 px-8 w-full">
                         <table className="table-gray">
-                            <tr className="table-tr-thead-gray">
-                                <th className="table-th-gray w-[30%]">Name</th>
-                                <th className="table-th-gray w-[30%]">Email</th>
-                                <th className="table-th-gray w-[10%]">Position</th>
-                                <th className="table-th-gray w-[10%]">Edit Working Hours</th>
-                                <th className="table-th-gray w-[10%]">Delete Employee</th>
-                                <th className="table-th-gray w-[10%]">Generate Token</th>
-                            </tr>
+                            <thead>
+                                <tr>
+                                    <th className="w-[30%]">Name</th>
+                                    <th className="w-[30%]">Email</th>
+                                    <th className="w-[10%]">Position</th>
+                                    <th className="w-[10%]">Edit Working Hours</th>
+                                    <th className="w-[10%]">Delete Employee</th>
+                                    <th className="w-[10%]">Generate Token</th>
+                                </tr>
+                            </thead>
                             <tbody>{generateEmployeeRows(employees)}</tbody>
                         </table>
                     </div>
                 </div>
             </div>
         </div>
-    );
+    )
 }
