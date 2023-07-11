978f5bd380afa4b13a9eb5c28de4679465852873
ngshijun <shijunng2010@gmail.com>
2023-07-11 14:36:55 +0800

    refactor

--- src/pages/Queue.js
+++ src/pages/Queue.js
@@ -7,38 +7,29 @@ import SideBar from "../components/SideBar"
 import BillingForm from "../components/BillingForm"
 
 export const Queue = () => {
-    const navigate = useNavigate();
+    const navigate = useNavigate()
     // Functions in AuthContext
-    const { logout } = useAuth();
+    const { logout } = useAuth()
 
     // Variables in DatabaseContext
-    const { allQueue, waitingQueue, inProgressQueue, pendingBillingQueue, completedQueue } = useDatabase();
+    const { allQueue, waitingQueue, inProgressQueue, pendingBillingQueue, completedQueue } = useDatabase()
 
     // Functions in DatabaseContext
-    const { updatePatientStatus } = useDatabase();
+    const { updatePatientStatus } = useDatabase()
 
-    const [filter, setFilter] = useState("waiting");
+    const [filter, setFilter] = useState("waiting")
 
-    const [search, setSearch] = useState("");
+    const [search, setSearch] = useState("")
 
     function generateQueue(queues) {
         return queues
             .filter((item) => {
                 return search.toLocaleLowerCase() === ""
                     ? item
-                    : item.data().patientName.toLocaleLowerCase().includes(search);
+                    : item.data().patientName.toLocaleLowerCase().includes(search)
             })
             .map((row) => (
-                <tr
-                    className={`cursor-pointer
-                ${onFocusIndex === row.id ? "tr-focus" : ""}`}
-                    key={row.id}
-                    onClick={() => {
-                        row.id === onFocusIndex ? setOnFocusIndex(null) : setOnFocusIndex(row.id);
-                    }}
-                    // The onDoubleClick is for testing purposes. It will be removed later.
-                    onDoubleClick={() => handlePatientProfile(row.data().patientId, row.id)}
-                >
+                <tr key={row.id}>
                     <td className="w-[20%]">{row.data().patientName}</td>
                     <td className="w-[10%]">{row.data().gender}</td>
                     <td className="w-[5%] ">{row.data().age}</td>
@@ -46,70 +37,70 @@ export const Queue = () => {
                     <td className="w-[30%]">{row.data().complains}</td>
                     <td className="w-[10%]">{row.data().status}</td>
                     <td className="w-[7%]">
-                    {row.data().status === "waiting" && (
-                        <button
-                            className="hover:text-green-500"
-                            onClick={(e) => {
-                                handlePatientCall(row.data().patientId, row.id)
-                            }}
-                        >
-                            <span class="">
+                        {row.data().status === "waiting" && (
+                            <button
+                                className="hover:text-green-500"
+                                onClick={(e) => {
+                                    handlePatientCall(row.data().patientId, row.id)
+                                }}
+                            >
+                                <span class="">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth="1.5"
+                                        stroke="currentColor"
+                                        class="w-6 h-6"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"
+                                        />
+                                    </svg>
+                                </span>
+                            </button>
+                        )}
+                        {row.data().status === "in progress" && (
+                            <button
+                                className="hover:text-green-500"
+                                onClick={(e) => {
+                                    handlePatientProfile(row.data().patientId, row.id)
+                                }}
+                            >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
                                     viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
+                                    stroke-width="1.5"
                                     stroke="currentColor"
                                     class="w-6 h-6"
                                 >
                                     <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
                                     />
                                 </svg>
-                            </span>
-                        </button>
-                    )}
-                    {row.data().status === "in progress" && (
-                        <button
-                            className="hover:text-green-500"
-                            onClick={(e) => {
-                                handlePatientProfile(row.data().patientId, row.id)
-                            }}
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                class="w-6 h-6"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
-                                />
-                            </svg>
-                        </button>
-                    )}
-                    {row.data().status === "pending billing" && (
-                        <BillingForm
-                            queueId={row.id}
-                            patientId={row.data().patientId}
-                            patientName={row.data().patientName}
-                        />
-                    )}
-                </td>
+                            </button>
+                        )}
+                        {row.data().status === "pending billing" && (
+                            <BillingForm
+                                queueId={row.id}
+                                patientId={row.data().patientId}
+                                patientName={row.data().patientName}
+                            />
+                        )}
+                    </td>
                 </tr>
-            ));
+            ))
     }
 
     const handlePatientCall = async (patientId, queueId) => {
-        await updatePatientStatus(queueId, "in progress");
-        handlePatientProfile(patientId, queueId);
-    };
+        await updatePatientStatus(queueId, "in progress")
+        handlePatientProfile(patientId, queueId)
+    }
 
     const handlePatientProfile = (patientId, queueId) => {
         navigate("/PatientProfile", { state: { patientId: patientId, mode: "consult", queueId: queueId } })
@@ -117,16 +108,16 @@ export const Queue = () => {
 
     async function handleLogout() {
         try {
-            await logout();
-            navigate("/");
+            await logout()
+            navigate("/")
         } catch (error) {
-            console.log(error);
+            console.log(error)
         }
     }
 
     const handleFilter = (e) => {
-        setFilter(e.target.value);
-    };
+        setFilter(e.target.value)
+    }
 
     return (
         <div className="flex flex-col h-full">
@@ -180,7 +171,7 @@ export const Queue = () => {
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Queue;
+export default Queue
